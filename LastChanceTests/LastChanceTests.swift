7982afb671c920215f33b005f22adbf47a70a821
Youppi3 <youppi3@gmail.com>
2016-02-09 19:43:13 -0600

    tutorial complete

---
+++ LastChanceTests/LastChanceTests.swift
@@ -0,0 +1,28 @@
+//
+//  LastChanceTests.swift
+//  LastChanceTests
+//
+//  Created by Ben Chaimberg on 2/5/16.
+//  Copyright © 2016 Ben Chaimberg. All rights reserved.
+//
+
+import UIKit
+import XCTest
+@testable import LastChance
+
+class LastChanceTests: XCTestCase {
+    
+    // MARK: FoodTracker Tests
+    // Tests to confirm that the Meal initializer returns when no name or a negative rating is provided.
+    func testMealInitialization() {
+        // Success case.
+        let potentialItem = Meal(name: "Newest meal", photo: nil, rating: 5)
+        XCTAssertNotNil(potentialItem)
+        // Failure cases.
+        let noName = Meal(name: "", photo: nil, rating: 0)
+        XCTAssertNil(noName, "Empty name is invalid")
+        let badRating = Meal(name: "Really bad rating", photo: nil, rating: -1)
+        XCTAssertNil(badRating, "Negative ratings are invalid, be positive")
+    }
+    
+}
